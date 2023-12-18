136456793a6f95259d2e4e8fe6024c89774610b7
Marcelo da Silva Cruz <mcruz@uesc.br>
2023-12-18 16:57:20 -0300

    commit atualização

--- FEB-P006/UESC-app/src/app/app.module.ts
+++ FEB-P006/UESC-app/src/app/app.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
-import { NoticiasComponent } from './noticias/noticias.component';
-import { DestaqueComponent } from './destaque/destaque.component';
-import { ServicosComponent } from './servicos/servicos.component';
-import { ResultadosComponent } from './resultados/resultados.component';
-import { FooterComponent } from './footer/footer.component';
+import { UescComponent } from './uesc/uesc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavComponent,
-    NoticiasComponent,
-    DestaqueComponent,
-    ServicosComponent,
-    ResultadosComponent,
-    FooterComponent
+    UescComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
